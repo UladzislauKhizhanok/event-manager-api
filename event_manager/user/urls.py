a1fc342fae2e5076404284b88d91ccbce130d0f5
Vladislav Hizhenok <uladzislau.khizhanok@gmail.com>
2024-08-26 10:55:35 +0200

    Add token verify endpoint

--- event_manager/user/urls.py
+++ event_manager/user/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 from user.views import RegisterView, UserView
 
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", UserView.as_view(), name="me"),
     path("register/", RegisterView.as_view(), name="register"),
 ]
